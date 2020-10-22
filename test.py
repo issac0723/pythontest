0035e10fb92f5f8bb4832284e6b7f6e585f52c17
drabdrab <zhaoyiming1226@gmail.com>
2020-10-21 22:21:29 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,17 +1,9 @@
 List = [] 
-num1 = int(input())
-num2 = int(input())
-num3 = int(input())
-num4 = int(input())
-num5 = int(input())
+for i in range(0,5):
+	num=int(input())
+	List.append(num)
 num6 = int(input())
-List.append(num1)
-List.append(num2)
-List.append(num3)
-List.append(num4)
-List.append(num5)
 print(List)
 print('['+str(List[0])+', '+str(List[1])+', '+str(List[2])+']')
 print('['+str(List[-2])+', '+str(List[-1])+']')
 print('The number', num6 ,'appears', List.count(num6) ,'time(s) in the list.')
-
