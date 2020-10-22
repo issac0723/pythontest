39e58f1579263ba3125069f875d7014b7af5b01b
drabdrab <zhaoyiming1226@gmail.com>
2020-10-21 21:47:41 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-print("Hello World")
+#print("Hello World")
 List = [] 
 num1 = int(input())
 num2 = int(input())
@@ -15,3 +15,4 @@ print(List)
 print('['+str(List[0])+', '+str(List[1])+', '+str(List[2])+']')
 print('['+str(List[-2])+', '+str(List[-1])+']')
 print('The number', num6 ,'appears', List.count(num6) ,'time(s) in the list.')
+print('test')
